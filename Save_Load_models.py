225758472599fbd7cdd6c7e16a62d1f1b8fb7299
Seong Su Kim <pand_a@naver.com>
2021-02-05 17:24:24 +0900

    Transfer_learning

---
+++ Save_Load_models.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torch.utils.data import dataloader
+from torch.utils.data.dataloader import DataLoader
+
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+class NN(nn.Module):
+
+    def __init__(self,input_size, num_classes):
+        super(NN,self).__init__() 
+
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self,x):
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+class CNN(nn.Module):
+
+    def __init__(self,in_channels = 1, num_classes = 10): # input_size -> in_channels 
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 8, kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.pool = nn.MaxPool2d(kernel_size = (2,2), stride = (2,2)) 
+        self.conv2 = nn.Conv2d(in_channels = 8, out_channels = 16, kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.fc1 = nn.Linear( 16 * 7 * 7, num_classes) # 28 / 4 -> Maxpool , Stride
+    
+    def forward(self,x):
+
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1) # x.shape[0] = batch size
+
+        x = self.fc1(x)
+
+        return x
+         
+        # n_in = number of input features
+        # k  = convolution kernel size
+        # p  = convolution padding size
+        # s  = convolution stride size
+
+        # n_out = number of output features = [n_in + 2p -k ]/s +1
+
+# Device
+def save_checkpoint(state,filename = "my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state,filename)
+
+def load_checkpoint(checkpoint):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyperparams
+# in_channels, num_classes, batch_size, epochs, learning_rate
+in_channels = 1 # input_size -> in_channels
+num_classes = 10
+batch_size = 64
+learning_rate = 0.001
+num_epochs = 10
+load_model = True
+# Data
+
+train_dataset = datasets.MNIST(root = '/home/sungsu21/Project/data', train = True, transform = transforms.ToTensor(),  download = False)
+train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle =True)
+
+test_dataset = datasets.MNIST(root = '/home/sungsu21/Project/data', train = False, transform = transforms.ToTensor(),  download = False)
+test_loader =  DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = True)
+
+# Initializing network
+
+model = CNN(in_channels = in_channels , num_classes = num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
+for epoch in range(num_epochs):
+    # Save model
+    losses = []
+    if epoch % 2 == 0:
+        checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+        save_checkpoint(checkpoint)
+        
+    
+    for batch_idx, (data,targets) in enumerate(train_loader):
+        
+        # Get data to cuda
+        data = data.to(device = device)
+        targets = targets.to(device = device)
+
+        # forward
+        scores = model(data)
+        loss = criterion(scores,targets)
+        losses.append(loss.item())
+        # backward
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        # gradiennt descent or adam step
+        optimizer.step()
+    print(f'Cost at epoch {epoch} is {sum(losses)/len(losses):.5f}')
+
+def Check_Accuracy(loader,model):
+    if loader.dataset.train:
+        print("Checking accuaracy on train data")
+    else:
+        print("Checking accuaracy on test data")
+    
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x,y in loader :
+            x = x.to(device = device)
+            y = y.to(device = device)
+
+            scores = model(x)
+            # scores : 64 X 10-->> Find maximum probability class
+            # scores.max(1) --> (values, indices)
+            _, predictions = scores.max(1) # We are interested in indices 
+
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(f'Got {num_correct} / {num_samples} with accuracy  {float(num_correct)/float(num_samples)*100:.2f}')
+        model.train()
+
+Check_Accuracy(train_loader,model)
+Check_Accuracy(test_loader,model)
---
+++ Transfer_learning_and_Fine_tuning.py
@@ -0,0 +1,194 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import dataloader
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+# Set device
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyperparams
+
+in_channels = 3
+num_classes = 10
+batch_size = 1024
+num_epochs = 5
+learning_rate = 1e-3
+
+import sys
+# Load pretrain model & modify it  -> Torchvision.models
+# Docs
+# Some models use modules which have different training and evaluation behavior, such as batch normalization.
+# To switch between these modes, use model.train() or model.eval() as appropriate. See train() or eval() for details.
+# All pre-trained models expect input images normalized in the same way, i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W are expected to be at least 224. 
+# The images have to be loaded in to a range of [0, 1] and then normalized using mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225]. You can use the following transform to normalize:
+model = torchvision.models.vgg16(pretrained = True) # Download in /home/sungsu21/.cache/torch/hub/checkpoints
+
+print(model) # Model Architecture
+
+#
+# VGG(
+#   (features): Sequential(
+#     (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (1): ReLU(inplace=True)
+#     (2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (3): ReLU(inplace=True)
+#     (4): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (5): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (6): ReLU(inplace=True)
+#     (7): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (8): ReLU(inplace=True)
+#     (9): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (10): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (11): ReLU(inplace=True)
+#     (12): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (13): ReLU(inplace=True)
+#     (14): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (15): ReLU(inplace=True)
+#     (16): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (17): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (18): ReLU(inplace=True)
+#     (19): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (20): ReLU(inplace=True)
+#     (21): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (22): ReLU(inplace=True)
+#     (23): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (24): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (25): ReLU(inplace=True)
+#     (26): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (27): ReLU(inplace=True)
+#     (28): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (29): ReLU(inplace=True)
+#     (30): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#   )
+#   (avgpool): AdaptiveAvgPool2d(output_size=(7, 7))
+#   (classifier): Sequential(
+#     (0): Linear(in_features=25088, out_features=4096, bias=True)
+#     (1): ReLU(inplace=True)
+#     (2): Dropout(p=0.5, inplace=False)
+#     (3): Linear(in_features=4096, out_features=4096, bias=True)
+#     (4): ReLU(inplace=True)
+#     (5): Dropout(p=0.5, inplace=False)
+#     (6): Linear(in_features=4096, out_features=1000, bias=True)
+#   )
+# )
+#
+# vgg16 last layer ->> Linear(in_features = 4096, out_features = 1000, bias = True)
+# We need to modify the classifer to have our features size to be 10
+# We don't need Avgpooling layer
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity,self).__init__()
+    def forward(self,x):
+        return x        
+
+for param in model.parameters():
+    param.requires_grad = False
+
+# Fine Tuning ---> training only last two layer, training goes a lot faster
+
+model.avgpool = Identity()
+#model.classifier = nn.Linear(512,10)
+model.classifier = nn.Sequential(nn.Linear(512,100),
+                                 nn.ReLU(),
+                                 nn.Linear(100,10))
+
+print(model)
+
+# VGG(
+#   (features): Sequential(
+#     (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (1): ReLU(inplace=True)
+#     (2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (3): ReLU(inplace=True)
+#     (4): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (5): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (6): ReLU(inplace=True)
+#     (7): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (8): ReLU(inplace=True)
+#     (9): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (10): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (11): ReLU(inplace=True)
+#     (12): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (13): ReLU(inplace=True)
+#     (14): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (15): ReLU(inplace=True)
+#     (16): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (17): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (18): ReLU(inplace=True)
+#     (19): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (20): ReLU(inplace=True)
+#     (21): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (22): ReLU(inplace=True)
+#     (23): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (24): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (25): ReLU(inplace=True)
+#     (26): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (27): ReLU(inplace=True)
+#     (28): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     (29): ReLU(inplace=True)
+#     (30): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#   )
+#   (avgpool): Identity()
+#   (classifier): Linear(in_features=512, out_features=10, bias=True)
+# )
+
+# Example
+
+# for i in range(1,7):
+#    model.classifier[i] = Identity()
+# print(model)
+
+
+# model.classifier = nn.Sequential(nn.Linear(512,100),
+#                                              nn.Dropout(p =0.5),
+#          
+
+model.to(device)
+
+# Dataset
+
+train_dataset = datasets.CIFAR10(root = '/home/sungsu21/Project/data',train = True, download = False, transform = transforms.ToTensor())
+train_loader = dataloader.DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
+
+test_dataset = datasets.CIFAR10(root = '/home/sungsu21/Project/data', train = False, download = False, transform = transforms.ToTensor())
+train_loader = dataloader.DataLoader(test_dataset, batch_size = batch_size, shuffle = True)
+
+
+# Criterion
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+
+
+# Train Network
+
+for epoch in range(num_epochs):
+    losses = []
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        
+        # Get data to cuda if possible
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+
+        # forward
+        scores = model(data)
+        loss = criterion(scores,targets)
+        
+        losses.append(loss.item())
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+        
+        # gradient descent 
+        optimizer.step()
+    
+    # mean loss per epoch
+    print(f'Cost at epoch {epoch} is {sum(losses)/len(losses)}')
+
+
